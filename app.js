0b85263ca5ac301015c9f84d26795eb0a2c6d5e8
alvainnala <128695531+alvainnala@users.noreply.github.com>
2024-08-05 17:30:06 +0300

    cleaning code

--- app.js
+++ app.js
@@ -2,28 +2,30 @@ const express = require('express');
 require('dotenv').config();
 const axios = require('axios');
 const NodeCache = require('node-cache');
-const app = express();
 
+const app = express();
 const myCache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 app.use(express.json());
 
 app.get('/external-api-data', async (req, res) => {
-    const cacheKey = 'externalData';
-    const cachedData = myCache.get(cacheKey);
+    const CACHE_KEY = 'externalData';
+    const cachedData = myCache.get(CACHE_KEY);
 
     if (cachedData) {
         console.log('Returning cached data');
-        return res.json(cachedData);
+        res.json(cachedData);
     } else {
         try {
-            const response = await axios.get('https://api.externalwebsite.com/data');
-            myCache.set(cacheKey, response.data);
+            const EXTERNAL_API_URL = 'https://api.externalwebsite.com/data';
+            const response = await axios.get(EXTERNAL_API_URL);
+
+            myCache.set(CACHE_KEY, response.data);
             console.log('Cache miss - Fetching new data');
-            return res.json(response.data);
+            res.json(response.data);
         } catch (error) {
             console.error('Error fetching external API data:', error);
-            return res.status(500).json({ error: 'Internal Server Error' });
+            res.status(500).json({ error: 'Internal Server Error' });
         }
     }
 });
