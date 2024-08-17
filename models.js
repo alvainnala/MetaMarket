8769309d7a9689f9d47a2153c83bb297b6c9d182
alvainnala <128695531+alvainnala@users.noreply.github.com>
2024-08-17 13:10:17 +0300

    update with new feature

--- models.js
+++ models.js
@@ -8,15 +8,31 @@ const itemSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   description: String,
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  ratings: [{
+    ratedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    rating: Number,
+  }],
+  averageRating: { type: Number, default: 0 }
 });
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: String,
   email: { type: String, unique: true, required: true },
-  signUpDate: { type: Date, default: Date.now }
+  signUpDate: { type: Date, default: Date.now },
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }]
 });
+
+itemSchema.methods.calculateAvgRating = function() {
+  if (this.ratings.length > 0) {
+    const sum = this.ratings.reduce((acc, cur) => acc + cur.rating, 0);
+    this.averageRating = sum / this.ratings.length;
+  } else {
+    this.averageRating = 0;
+  }
+  return this.save();
+};
 
 const Item = mongoose.model('Item', itemSchema);
 const User = mongoose.model('User', userSchema);
