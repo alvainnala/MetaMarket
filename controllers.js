6935d190e15a843f48e3f4ca2e6b30d2ccadd435
alvainnala <128695531+alvainnala@users.noreply.github.com>
2024-08-24 23:14:43 +0300

    add new variables

--- controllers.js
+++ controllers.js
@@ -6,46 +6,43 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+app.use(express.json()); // This replaces body-parser for parsing JSON
 
-const users = [];
-const items = [];
+const users = new Map();
+const items = []; 
 
-function generateAccessToken(user) {
-  return jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+function generateAccessToken(username) {
+  return jwt.sign({ username }, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
 }
 
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401)
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 
 app.post('/register', (req, res) => {
   const { username, password } = req.body;
-  if(!username || !password) {
+  if (!username || !password) {
     return res.status(400).send('Username and password are required');
   }
-  const userExists = users.find(user => user.username === username);
-  if(userExists) {
+  if (users.has(username)) {
     return res.status(400).send('User already exists');
   }
-  const newUser = { username, password };
-  users.push(newUser);
+  users.set(username, password); 
   res.send('User registered successfully');
 });
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
-  const user = users.find(user => user.username === username && user.password === password);
-  if(user) {
-    const token = generateAccessToken({ username });
+  if (users.get(username) === password) {
+    const token = generateAccessToken(username);
     res.json({ accessToken: token });
   } else {
     res.send('Username or password incorrect');
@@ -53,10 +50,11 @@ app.post('/login', (req, res) => {
 });
 
 app.post('/items', authenticateToken, (req, res) => {
-  const item = req.body;
-  if(!item.name || !item.description) {
+  const { name, description } = req.body;
+  if (!name || !description) {
     return res.status(400).send('Item name and description are required');
   }
+  const item = { name, description }; 
   items.push(item);
   res.send('Item added successfully');
 });
