244b95c41ad377805ce680c94e6c39b87553c669
alvainnala <128695531+alvainnala@users.noreply.github.com>
2024-09-27 16:24:23 +0300

    optimization

--- auth.js
+++ auth.js
@@ -1,38 +1,24 @@
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache();
 
-dotenv.config();
-
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  
-  if (!authHeader) {
-    return res.status(401).json({ error: "Authorization header is missing" });
+const fetchData = async (key, fetchFunction) => {
+  const value = myCache.get(key);
+  if (value) {
+    return Promise.resolve(value);
+  } else {
+    const data = await fetchFunction();
+    myCache.set(key, data);
+    return data;
   }
+}
 
-  const tokenParts = authHeader.split(" ");
-  
-  if (tokenParts.length !== 2 || tokenParts[0] !== "Bearer") {
-    return res.status(401).json({ error: "Authorization header is malformed" });
+const getCachedData = async () => {
+  try {
+    const data = await fetchData('uniqueKeyForData', async () => {
+      return getDataFromAPI();
+    });
+    console.log(data);
+  } catch (error) {
+    console.error("Failed to fetch data", error);
   }
-
-  const token = tokenParts[1];
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) {
-      const message = err.name === 'TokenExpiredError' ? "Token expired" : "Token is invalid";
-      return res.status(403).json({ error: message });
-    }
-
-    try {
-      req.user = user;
-    } catch (innerErr) {
-      console.error(innerErr);
-      return res.status(500).json({ error: "An error occurred while processing the user data" });
-    }
-    
-    next();
-  });
 };
-
-module.exports = authenticateToken;
