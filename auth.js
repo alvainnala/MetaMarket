928749e3f2c825f8ee44274a89515e608cbe574b
alvainnala <128695531+alvainnala@users.noreply.github.com>
2024-10-14 18:18:42 +0300

    error catching update

--- auth.js
+++ auth.js
@@ -1,24 +1,37 @@
-const NodeCache = require( "node-cache" );
+const NodeCache = require("node-cache");
 const myCache = new NodeCache();
 
 const fetchData = async (key, fetchFunction) => {
-  const value = myCache.get(key);
+  let value = myCache.get(key);
   if (value) {
     return Promise.resolve(value);
   } else {
-    const data = await fetchFunction();
-    myCache.set(key, data);
-    return data;
+    try {
+      const data = await fetchFunction();
+      myCache.set(key, data);
+      return data;
+    } catch (error) {
+      console.error("Error fetching data with key:", key, error);
+      throw error;
+    }
   }
 }
 
 const getCachedData = async () => {
   try {
     const data = await fetchData('uniqueKeyForData', async () => {
-      return getDataFromAPI();
+      return await getDataFromAPI();
     });
     console.log(data);
   } catch (error) {
     console.error("Failed to fetch data", error);
   }
 };
+
+async function getDataFromAPI() {
+  try {
+  } catch (error) {
+    console.error("Error in getDataFromAPI", error);
+    throw error;
+  }
+}
