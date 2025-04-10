47a7be7837531f343fc4318ba3c5c8b6743af34c
alvainnala <128695531+alvainnala@users.noreply.github.com>
2025-04-10 17:08:57 +0300

    update error catching

--- client/App.tsx
+++ client/App.tsx
@@ -3,6 +3,8 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
+  useParams,
+  Link,
 } from 'react-router-dom';
 
 type AppProps = {};
@@ -13,18 +15,36 @@ const Storefront: React.FC = () => (
   </div>
 );
 
-const ItemDetail: React.FC = () => (
-  <div>
-    <h1>Item Detail View</h1>
-  </div>
-);
+const ItemDetail: React.FC = () => {
+  let { itemId } = useParams<{ itemId: string }>();
+
+  return (
+    <div>
+      <h1>Item Detail View</h1>
+      <p>Looking at item with ID: {itemId}</p>
+    </div>
+  );
+};
+
+const NoMatch: React.FC = () => {
+  return (
+    <div>
+      <h2>404 Page Not Found</h2>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <p>
+        Click <Link to="/">here</Link> to go back to the homepage.
+      </p>
+    </div>
+  );
+};
 
 const App: React.FC<AppProps> = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Storefront} />
-        <Route path="/item/:itemId" component={ItemDetail} />
+        <Route path="/item/:itemId" exact component={ItemDetail} />
+        <Route path="*" component={NoMatch} />
       </Switch>
     </Router>
   );
